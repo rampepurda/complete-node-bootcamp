ebbb3f2270dbf6438161ef8f98d9e1c669a60ebb
michal tomek <95964850+rampepurda@users.noreply.github.com>
2025-01-20 10:08:44 +0100

    feat: i18config .use(Backend)

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+/**
+ * @param Suspense - was used because of determinate translation.json file(public)
+ */
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <React.Suspense fallback="loading">
+      <App />
+    </React.Suspense>
   </React.StrictMode>
 )
--- frontend/src/pages/RootLayout.tsx
+++ frontend/src/pages/RootLayout.tsx
@@ -1,17 +1,11 @@
-import { Header, LngSwitcher } from '../Components'
+import { Header } from '../Components'
 import DocsPage from './docs'
 import '../i18n/config'
-import { useTranslation } from 'react-i18next'
-import { useEffect } from 'react'
 
 export default function RootLayout() {
-  const { t } = useTranslation()
-  useEffect(() => {}, [t])
-
   return (
     <>
       <Header />
-      <LngSwitcher />
 
       <main>
         <DocsPage />
