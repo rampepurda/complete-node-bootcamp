c9bd18f36d00009619cef9ebec5aa13e52f46b39
michal tomek <95964850+rampepurda@users.noreply.github.com>
2025-02-14 11:54:42 +0100

    chore: Playlist | isComplMutation is commented, waiting for solution

--- frontend/src/reactPlayer/Components/Playlist.tsx
+++ frontend/src/reactPlayer/Components/Playlist.tsx
@@ -5,22 +5,29 @@ import { useMutation } from '@tanstack/react-query'
 import { environment } from '../../configuration/environment'
 import { Button } from '../../Components'
 
-export const Playlist = ({ url, title, id, isCompleted }: PlaylistT) => {
+export const Playlist = ({ url, title, id }: PlaylistT) => {
   const [isCompleteD, setIsCompleted] = useState<boolean>(false)
   const [toggleEditForm, setToggleEditForm] = useState<boolean>(false)
-  const isVideoCompletedMutation = useMutation({
-    mutationKey: [],
-    mutationFn: async (arg: { id: string | number; onEnded: string }) => {
-      await fetch(`${environment.localPlaylistURL}/${arg.id}`, {
-        method: 'PATCH',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(arg.onEnded),
-      })
-    },
-    onError: (err) => alert(err),
-  })
+
+  /**
+   * isVideoCompleted: In progress, status is Open
+   *  const isVideoCompletedMutation = useMutation({
+   *     mutationKey: [],
+   *     mutationFn: async (arg: { id: string | number; isCompleted: string }) => {
+   *       await fetch(`${environment.localPlaylistURL}/${arg.id}`, {
+   *         method: 'PATCH',
+   *         headers: { 'Content-Type': 'application/json' },
+   *         body: JSON.stringify(arg.isCompleted),
+   *       })
+   *     },
+   *     onError: (err) => alert(err),
+   *   })
+   *   async function videoIsCompleted() {
+   *     return isVideoCompletedMutation.mutate({ id, isCompleted: 'true' })
+   *   }
+   */
   const patchPlaylistMutation = useMutation({
-    mutationKey: [],
+    mutationKey: [`video${id}`],
     mutationFn: async (arg: { id: string | number; dataForm: Record<string, any> }) => {
       await fetch(`${environment.localPlaylistURL}/${arg.id}`, {
         method: 'PATCH',
@@ -34,13 +41,13 @@ export const Playlist = ({ url, title, id, isCompleted }: PlaylistT) => {
     const data = new FormData(event.currentTarget)
     const formData = Object.fromEntries(data)
 
-    patchPlaylistMutation.mutate({ id: id, dataForm: formData })
+    patchPlaylistMutation.mutate({ id, dataForm: formData })
   }
 
   return (
     <div className="hasOutline width-is-7">
       <h2>{title}</h2>
-      {isCompleted ? (
+      {isCompleteD ? (
         <p className="color-is-green">video is Completed</p>
       ) : (
         <p className="color-is-red">video is not Completed</p>
@@ -56,7 +63,7 @@ export const Playlist = ({ url, title, id, isCompleted }: PlaylistT) => {
 
         {toggleEditForm && (
           <form method="patch" onSubmit={handleSubmitStatus} aria-expanded={toggleEditForm}>
-            <input name="title" type="text" placeholder="change title" required={true} />
+            <input name="title" type="text" placeholder={title} required={true} />
             <button className="btn btn-submit" type="submit">
               Submit
             </button>
