cb9dc256e833d93373217a6ecf25ed3669e74f9c
michal tomek <95964850+rampepurda@users.noreply.github.com>
2025-06-02 11:24:30 +0200

    feat: eShop | useQuery signal: if paramSearch

--- frontend/src/pages/eShop/index.tsx
+++ frontend/src/pages/eShop/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Button, Product, SelectedSort } from '../../Components'
 import { useMutation, useQuery } from '@tanstack/react-query'
 import { ProductInt } from '../../types'
@@ -6,28 +6,48 @@ import { environment } from '../../configuration/environment'
 import { useTranslation } from 'react-i18next'
 import { useAppDispatch } from '../../rtk-toolkit/hooks'
 import { fetchCart } from '../../rtk-toolkit/slices/cartSlice'
-import { fetcher } from '../../utils/fetcher'
 import { useSearchParams } from 'react-router'
 import { optionsEshop } from '../../__mock__/mock_data'
 
 export function EShopPage() {
-  const [searchParams] = useSearchParams()
-  const querySort: string | null = searchParams.get('sort')
+  const [searchParams, setSearchParams] = useSearchParams()
+  const paramSort: string | null = searchParams.get('sort')
   const { t } = useTranslation()
   const dispatch = useAppDispatch()
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, error, refetch } = useQuery({
     queryKey: ['productsOrdered'],
-    queryFn: async (): Promise<
+    queryFn: async ({
+      signal,
+    }): Promise<
       | {
           products: ProductInt[] | undefined
           message: string
-          productsTotal: number
         }
       | undefined
-    > =>
-      await fetcher(`${environment.localProductsURL}?sort=${querySort}`, {
+    > => {
+      const response = await fetch(`${environment.localProductsURL}?sort=${paramSort}`, {
         method: 'GET',
-      }),
+        signal: signal,
+      })
+
+      if (paramSort) {
+        try {
+          return response.json()
+        } catch (err) {
+          alert(err)
+        }
+      } else {
+        try {
+          setSearchParams((prev) => {
+            return prev
+          })
+          return response.json()
+        } catch (err) {
+          alert(err)
+        }
+      }
+    },
+    enabled: paramSort === null || true,
   })
   const { mutate } = useMutation({
     mutationKey: ['cart'],
@@ -55,7 +75,7 @@ export function EShopPage() {
     <section style={{ margin: '1rem 5rem' }}>
       <SelectedSort options={optionsEshop} name={'sort'} btnClass={'btn-primary'} />
 
-      {isLoading && <h3>...loading, wait</h3>}
+      {(isLoading && <h3>...loading, wait</h3>) || (error && <h3>Ops, something happened</h3>)}
       <div className="display-grid display-grid-temp-columns-three">
         {data?.products?.map((product: ProductInt) => (
           <Product
